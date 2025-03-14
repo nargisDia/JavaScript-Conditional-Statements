ca7e02b7e7873ad4fd62829413ccf2cc300dde81
nargisDia <126104947+nargisDia@users.noreply.github.com>
2025-03-15 03:22:37 +0600

    Add files via upload

---
+++ Conditional_Statements_18/app.js
@@ -0,0 +1,49 @@
+// JavaScript Conditional Statements
+
+let age = 4;
+
+if (age>40) {
+  console.log(`You are old`);
+}
+else if (age > 18) {
+    console.log(`You are ready to open a Google Account`);
+
+}
+else if (age > 18) {
+    console.log(`You are ready to open a Google Account`);
+
+}
+else if (age > 18) {
+    console.log(`You are ready to open a Google Account`);
+
+}
+else if (age > 18) {
+    console.log(`You are ready to open a Google Account`);
+
+}
+
+else {
+    console.log(`Your are not ready`);
+}
+
+
+
+let category = 'Truck';
+let carType;
+
+
+switch (category) {
+    case 'car':
+        carType = `This is a car`;
+        break;
+    case 'Motor Bike':
+        carType = `This is a Motor Bike`;
+        break;
+        case 'Truck':
+            carType = `This is a Truck`;
+            break;
+    default:
+        carType = `Unknown Vehicle Category`;
+}
+
+console.log(carType);
